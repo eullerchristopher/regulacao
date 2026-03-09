bd3fd599ce7567424624103e525951295f864fce
Euller <eullerchristopher@gmail.com>
2026-03-09 07:30:40 -0300

    subi mais algumas modificações

--- buid.sh
+++ buid.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# instalar Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# instalar dependências
 pip install -r requirements.txt
+
+# rodar migrations e coletar static
 python manage.py migrate
 python manage.py collectstatic --noinput
+
