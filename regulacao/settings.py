8c2ca67ed3b1a73ac3bbf4a96ed67815865e792b
Euller <eullerchristopher@gmail.com>
2026-03-09 18:29:38 -0300

    atualizacao do settings.py

--- regulacao/settings.py
+++ regulacao/settings.py
@@ -44,6 +44,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'regulacao.urls'
 
 TEMPLATES = [
